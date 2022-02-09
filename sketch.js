78a80c1a8358db8453b3f4d37993032a1c3ea61b
bielg4m3r <99212550+bielg4m3r@users.noreply.github.com>
2022-02-09 15:47:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var caixa;
+
+function setup() {
+
+    createCanvas(600, 400)
+
+    caixa = createSprite (400,400,50,50)
+}
+
+function draw() {
+
+    background("blue");
+    drawSprites();
+
+
+
+
+
+
+}
